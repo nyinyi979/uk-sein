93f93417c6c0373ecbdfbf083938027d4f8c90eb
nyinyi979 <nyinyiaung@Nyis-MacBook-Air.local>
2024-07-10 20:40:42 +0630

    1.1

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -4,3 +4,7 @@ export const metadata: Metadata = {
   title: "Category",
   description: "Different categories of products available in UK SEIN!",
 };
+
+export default function(){
+  return(<></>)
+}
