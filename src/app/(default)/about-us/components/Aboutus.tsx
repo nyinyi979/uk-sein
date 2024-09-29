87e6ca9f00753f856f8d53c1643d80c9912d135c
zenith <zenith@visibleone.com>
2024-09-29 13:26:20 +0700

    update

--- src/app/(default)/about-us/components/Aboutus.tsx
+++ src/app/(default)/about-us/components/Aboutus.tsx
@@ -9,7 +9,7 @@ export default function AboutUS() {
   return (
     <div className="xl:w-[1192px] md:w-[85%] sm:w-[90%] w-full flex flex-col xl:gap-20 md:gap-10 gap-8 mx-auto pb-10">
       <AboutUsCards />
-      <FounderCard />
+      {/* <FounderCard /> */}
       <WhatDoWeServe />
       <SeasonalPromotions />
       <div className="md:block hidden">
--- src/app/(default)/portfolio/components/Portfolio.tsx
+++ src/app/(default)/portfolio/components/Portfolio.tsx
@@ -13,7 +13,7 @@ export default function Portfolio() {
       <OurRecentProjects />
       <PortfolioCards />
       <SeasonalPromotions />
-      <FounderCard />
+      {/* <FounderCard /> */}
     </div>
   );
 }
